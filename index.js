01f4077b8ecd0b0d2394fda5fd81fd88b4b5c30c
VrushaliDipakPatil <vrushalip910@gmail.com>
2026-07-17 17:38:21 +0530

    handle multiple users on localStorage

--- index.js
+++ index.js
@@ -1,14 +1,48 @@
-// Write your code below 
+// Write your code below:
 function handleFormSubmit(event) {
     event.preventDefault();
-    console.log(event);
+
     const username = event.target.username.value;
     const email = event.target.email.value;
     const phone = event.target.phone.value;
 
-    localStorage.setItem("username", username);
-    localStorage.setItem("email", email);
-    localStorage.setItem("phone", phone);
+    const user = {
+        username: username,
+        email: email,
+        phone: phone
+    };
+
+    localStorage.setItem(email, JSON.stringify(user));
+
+    showUserOnScreen(user);
+
+    event.target.username.value = "";
+    event.target.email.value = "";
+    event.target.phone.value = "";
+
+
 }
+
+function showUserOnScreen(user) {
+    const parent = document.getElementById('users');
+    const li = document.createElement('li');
+    li.textContent = `Username:${user.username}, Email:${user.email}, Phone: ${user.phone}`;
+    parent.appendChild(li);
+}
+
+function getUserFromLocalStorage() {
+    const parent = document.getElementById('users');
+    parent.innerHTML = "";
+
+    for (let i = 0; i < localStorage.length; i++){
+        const key = localStorage.key(i);
+
+        const user = JSON.parse(localStorage.getItem(key));
+
+        showUserOnScreen(user);
+    }
+}
+
+window.addEventListener("DOMContentLoaded", getUserFromLocalStorage);
 
 module.exports = handleFormSubmit;
