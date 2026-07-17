be909bf6fe31f4b9005aee00c6ad61bbd184a55e
VrushaliDipakPatil <vrushalip910@gmail.com>
2026-07-17 17:03:08 +0530

    understanding local storage

--- index.js
+++ index.js
@@ -1,78 +1,14 @@
-let form = document.getElementsByTagName('form')[0];
-
-const descriptionInput = document.createElement('input');
-descriptionInput.type = 'text';
-descriptionInput.id = 'description';
-descriptionInput.placeholder = 'Enter fruit description';
-
-const addButton = form.querySelector('button');
-form.insertBefore(descriptionInput, addButton);
-
-
-// add fruit to the cart
-let fruititems = document.querySelector('.fruits');
-let descriptionitems = document.querySelector('#description');
-
-form.addEventListener('submit', function (event) {
+// Write your code below 
+function handleFormSubmit(event) {
     event.preventDefault();
+    console.log(event);
+    const username = event.target.username.value;
+    const email = event.target.email.value;
+    const phone = event.target.phone.value;
 
-    let fruitName = document.getElementById('fruit-to-add').value;
-    let fruitDescription = document.getElementById('description').value;
-    if (fruitDescription === '') {
-        alert('Please enter a description.');
-        return;
-    }
+    localStorage.setItem("username", username);
+    localStorage.setItem("email", email);
+    localStorage.setItem("phone", phone);
+}
 
-    let li = document.createElement('li');
-    li.className = 'fruit';
-
-    li.appendChild(document.createTextNode(fruitName));
-
-    let p = document.createElement('p');
-    p.textContent = fruitDescription;
-    p.style.fontStyle = 'italic';
-    li.appendChild(p);
-
-    let deleteBtn = document.createElement('button');
-    deleteBtn.className = 'delete-btn';
-    deleteBtn.textContent = "X";
-
-    li.appendChild(deleteBtn);
-
-    fruititems.appendChild(li);
-
-    document.getElementById('fruit-to-add').value = '';
-    document.getElementById('description').value = '';
-});
-
-
-// delete functionality
-fruititems.addEventListener('click', function (event) {
-    if (event.target.classList.contains('delete-btn')) {
-        let buttontodelete = event.target.parentElement;
-        fruititems.removeChild(buttontodelete);
-    }
-});
-
-
-const filter = document.getElementById('filter');
-
-filter.addEventListener('keyup', function (event) {
-    const text = event.target.value.toLowerCase();
-    const fruits = document.querySelectorAll('.fruit');
-    fruits.forEach(function (fruit) {
-        const fruitName = fruit.childNodes[0].textContent.toLowerCase();
-
-        const description = fruit.querySelector('p') ?
-            fruit.querySelector('p').textContent.toLowerCase() : '';
-
-        if (fruitName.includes(text) || description.includes(text)) {
-            fruit.style.display = 'flex';
-        } else {
-            fruit.style.display = 'none';
-            }
-    })
-});
-
-
-
+module.exports = handleFormSubmit;
